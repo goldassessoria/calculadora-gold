cdd5540c62c9d2840f2aa334b3ca81cfeeb2ff6d
Gold Assessoria Marketing Para Pizzarias <info.goldassessoria@gmail.com>
2025-09-15 00:00:30 +0000

    como LOGO no cabeçalho, use essa imagem de forma centralizada: https://i

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
-    <header className="py-8">
-      {/* O conteúdo do cabeçalho será adicionado aqui posteriormente */}
+    <header className="py-8 flex justify-center">
+      <Image
+        src="https://i.imgur.com/UBxesF1.png"
+        alt="Calculadora Gold Logo"
+        width={200}
+        height={50}
+        priority
+      />
     </header>
   );
 }
