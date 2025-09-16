8a78847466866fd2ed670700bf9f3e5f09cf4121
Gold Assessoria Marketing Para Pizzarias <info.goldassessoria@gmail.com>
2025-09-16 02:28:26 +0000

    transforme a logo do cabeçalho em um elemento clicável, e ao clicar redi

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="py-8 flex justify-center">
-      <Image
-        src="https://i.imgur.com/UBxesF1.png"
-        alt="Calculadora Gold Logo"
-        width={200}
-        height={50}
-        priority
-      />
+      <a href="https://goldpizzarias.com.br" target="_blank" rel="noopener noreferrer">
+        <Image
+          src="https://i.imgur.com/UBxesF1.png"
+          alt="Calculadora Gold Logo"
+          width={200}
+          height={50}
+          priority
+        />
+      </a>
     </header>
   );
 }
